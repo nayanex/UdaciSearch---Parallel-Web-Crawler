74622def37bb8d0cb17abc33e1a66bf72faca1fe
Nayana Holanda de Abreu <holanda.nayana@gmail.com>
2023-07-22 15:58:57 +0200

    Performance Profiler

--- src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -3,12 +3,15 @@ package com.udacity.webcrawler.profiler;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.io.Writer;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Clock;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
 import java.util.Objects;
 
 import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
+import java.lang.reflect.Proxy;
 
 /**
  * Concrete implementation of the {@link Profiler}.
@@ -28,18 +31,37 @@ final class ProfilerImpl implements Profiler {
   @Override
   public <T> T wrap(Class<T> klass, T delegate) {
     Objects.requireNonNull(klass);
-
     // TODO: Use a dynamic proxy (java.lang.reflect.Proxy) to "wrap" the delegate in a
     //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
     //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
 
-    return delegate;
+    Objects.requireNonNull(delegate);
+
+    // Check if the wrapped interface contains any methods annotated with @Profiled
+    boolean hasProfiledMethod = Arrays.stream(klass.getMethods())
+            .anyMatch(method -> method.isAnnotationPresent(Profiled.class));
+
+    if (!hasProfiledMethod) {
+      throw new IllegalArgumentException("The wrapped interface must contain a @Profiled method.");
+    }
+
+    return klass.cast(
+            java.lang.reflect.Proxy.newProxyInstance(
+                    klass.getClassLoader(),
+                    new Class<?>[]{klass},
+                    new ProfilingMethodInterceptor<>(delegate, clock, state)));
   }
 
   @Override
-  public void writeData(Path path) {
+  public void writeData(Path path) throws IOException {
     // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
     //       path, the new data should be appended to the existing file.
+    try (Writer writer = Files.newBufferedWriter(path)) {
+      writer.write("Run at " + RFC_1123_DATE_TIME.format(startTime));
+      writer.write(System.lineSeparator());
+      state.write(writer);
+      writer.write(System.lineSeparator());
+    }
   }
 
   @Override
--- src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
+++ src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -3,28 +3,52 @@ package com.udacity.webcrawler.profiler;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Objects;
 
 /**
  * A method interceptor that checks whether {@link Method}s are annotated with the {@link Profiled}
  * annotation. If they are, the method interceptor records how long the method invocation took.
  */
-final class ProfilingMethodInterceptor implements InvocationHandler {
+final class ProfilingMethodInterceptor<T> implements InvocationHandler {
 
+  private final T target;
   private final Clock clock;
+  private final ProfilingState profilingState;
 
   // TODO: You will need to add more instance fields and constructor arguments to this class.
-  ProfilingMethodInterceptor(Clock clock) {
+  public ProfilingMethodInterceptor(T target, Clock clock, ProfilingState profilingState) {
+    this.target = Objects.requireNonNull(target);
     this.clock = Objects.requireNonNull(clock);
+    this.profilingState = Objects.requireNonNull(profilingState);
   }
 
   @Override
-  public Object invoke(Object proxy, Method method, Object[] args) {
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
     // TODO: This method interceptor should inspect the called method to see if it is a profiled
     //       method. For profiled methods, the interceptor should record the start time, then
     //       invoke the method using the object that is being profiled. Finally, for profiled
     //       methods, the interceptor should record how long the method call took, using the
     //       ProfilingState methods.
-    return null;
+
+    // Check if the method is annotated with @Profiled
+    if (!method.isAnnotationPresent(Profiled.class)) {
+      return method.invoke(target, args);
+    }
+
+    Instant beforeInvocation = clock.instant();
+    try {
+      Object result = method.invoke(target, args);
+      Instant afterInvocation = clock.instant();
+      Duration duration = Duration.between(beforeInvocation, afterInvocation);
+      profilingState.record(target.getClass(), method, duration);
+      return result;
+    } catch (Throwable t) {
+      Instant afterInvocation = clock.instant();
+      Duration duration = Duration.between(beforeInvocation, afterInvocation);
+      profilingState.record(target.getClass(), method, duration);
+      throw t.getCause() != null ? t.getCause() : t;
+    }
   }
 }
